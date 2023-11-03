a6b87e4a4356c1b1126f3ad31cb5736417bbb37c
Alexandre Gutierry <aleguti1000@hotmail.com>
2023-11-03 10:10:22 -0300

    projects page

--- src/app/portifolio/page.tsx
+++ src/app/portifolio/page.tsx
@@ -13,16 +13,20 @@ export default async function Portifolio() {
         <p className="typeNav">Back End</p>
         <p className="typeNav">Mobile</p>
       </nav>
-      <div className="flex flex-wrap justify-space-around items-center">
+      <div className="flex flex-wrap justify-space-around items-center gap-8 mt-6">
         {projects.map((project) => (
-          <Link key={`${project.id}`} href={`portifolio/project/${project.id}`}>
+          <Link
+            className="hover:scale-110 hover:shadow-2xl hoover:shadow-black hover:opacity-95 transition-all duration-300"
+            key={`${project.id}`}
+            href={`portifolio/project/${project.id}`}
+          >
             <Image
               src={`/${project.image}`}
               alt={project.name}
               width={400}
               height={200}
             />
-            <p className="text-center">{project.name}</p>
+            <p className="text-center text-xl mt-2 ">{project.name}</p>
           </Link>
         ))}
       </div>
